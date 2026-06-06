ccc628bac2c483cf5137e790e4d6f309d8aa2a40
SmallMovingParticle <chaudharydivyansh04@gmail.com>
2026-06-06 13:09:25 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+##responsible for crating ml project as a package
+
+from setuptools import find_packages , setup
+from typing import List
+
+
+HYPEN_E_DOT = '_e .'
+def get_requirements(file_path:str)->List[str]:
+    ##this fuction return the list of requirements 
+
+    requirements = []
+    with open (file_path) as file_obj:
+        requriments = file_obj.readlines()
+        [req.replace ("\n"," ")   for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name= 'mlProject',
+    version= '0.0.1',
+    author= 'Devyansh Chaudhary',
+    author_email='chaudharydivyansh04@gmail.com',
+    packages= find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
